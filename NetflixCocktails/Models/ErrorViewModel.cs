3b1e4c463a64e52f3c08f6bffdf64348854ad96f
oliviaharlin <oliviaharlin@hotmail.com>
2022-05-09 11:23:59 +0200

    hej

---
+++ NetflixCocktails/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NetflixCocktails.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
