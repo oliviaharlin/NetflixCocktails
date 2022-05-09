2cedfee2cf850f89077c6af7b3f92d7af4a64a29
oliviaharlin <oliviaharlin@hotmail.com>
2022-05-09 16:12:19 +0200

    Fixade med att generera en cocktail

--- NetflixCocktails/Controllers/HomeController.cs
+++ NetflixCocktails/Controllers/HomeController.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NetflixCocktails.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using static NetflixCocktails.Helper.Helper;
 
 //TESTKOMMENTAR AV OLIVIA
 //LISA TESTAR
@@ -13,6 +17,10 @@ namespace NetflixCocktails.Controllers
 {
     public class HomeController : Controller
     {
+
+        CocktailAPI _api = new CocktailAPI();
+
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -20,10 +28,107 @@ namespace NetflixCocktails.Controllers
             _logger = logger;
         }
 
+        /*
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        } */
+
+
+        public async Task<IActionResult> Index()
+        {
+
+           Cocktail cocktail = new Cocktail();
+
+            
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("http://www.thecocktaildb.com/api/json/v1/1/random.php");
+
+            if (res.IsSuccessStatusCode) {
+            
+
+                string responseBody = await res.Content.ReadAsStringAsync();
+
+                JObject googleSearch = JObject.Parse(responseBody);
+
+                IList<JToken> results = googleSearch["drinks"].Children().ToList();
+
+                IList<Cocktail> searchResults = new List<Cocktail>();
+                foreach (JToken result in results)
+                {
+                    Cocktail cocktail1 = result.ToObject<Cocktail>();
+                    searchResults.Add(cocktail1);
+                }
+
+
+                List<String> ingredients = new List<string>();
+                List<String> measurements = new List<string>();
+                List<String> content = new List<string>();
+                ingredients.Add(searchResults[0].strIngredient1);
+                ingredients.Add(searchResults[0].strIngredient2);
+                ingredients.Add(searchResults[0].strIngredient3);
+                ingredients.Add(searchResults[0].strIngredient4);
+                ingredients.Add(searchResults[0].strIngredient5);
+                ingredients.Add(searchResults[0].strIngredient6);
+                ingredients.Add(searchResults[0].strIngredient7);
+                ingredients.Add(searchResults[0].strIngredient8);
+                ingredients.Add(searchResults[0].strIngredient9);
+                ingredients.Add(searchResults[0].strIngredient10);
+                ingredients.Add(searchResults[0].strIngredient11);
+                ingredients.Add(searchResults[0].strIngredient12);
+                ingredients.Add(searchResults[0].strIngredient13);
+                ingredients.Add(searchResults[0].strIngredient14);
+                ingredients.Add(searchResults[0].strIngredient15);
+
+                measurements.Add(searchResults[0].strMeasure1);
+                measurements.Add(searchResults[0].strMeasure2);
+                measurements.Add(searchResults[0].strMeasure3);
+                measurements.Add(searchResults[0].strMeasure4);
+                measurements.Add(searchResults[0].strMeasure5);
+                measurements.Add(searchResults[0].strMeasure6);
+                measurements.Add(searchResults[0].strMeasure7);
+                measurements.Add(searchResults[0].strMeasure8);
+                measurements.Add(searchResults[0].strMeasure9);
+                measurements.Add(searchResults[0].strMeasure10);
+                measurements.Add(searchResults[0].strMeasure11);
+                measurements.Add(searchResults[0].strMeasure12);
+                measurements.Add(searchResults[0].strMeasure13);
+                measurements.Add(searchResults[0].strMeasure14);
+                measurements.Add(searchResults[0].strMeasure15);
+
+
+
+                for (int i=0; i<15; i++)
+                {
+                    string ingredient = ingredients[i];
+                    string measurement = measurements[i];
+                    if(ingredient != null || measurement != null)
+                    {
+                        string line = measurement + " " + ingredient;
+                        content.Add(line);
+
+                    }
+               
+                }
+
+                searchResults[0].ingredientList = content;
+
+
+                return View(searchResults[0]);
+
+            }
+
+            ViewBag.error = "null";
+
+            return View();
+
+
+            
         }
+
+
+
 
         public IActionResult Privacy()
         {
---
+++ NetflixCocktails/Models/Cocktail.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace NetflixCocktails.Models
+{
+    [JsonObject()]
+    public class Cocktail
+    {
+
+        [JsonProperty("strDrink")]
+        public string strDrink { get; set; }
+
+        [JsonProperty("strCategory")]
+        public string strCategory { get; set; }
+
+        [JsonProperty("strAlcoholic")]
+        public string strAlcoholic { get; set; }
+
+        [JsonProperty("strGlass")]
+        public string strGlass { get; set; }
+
+        [JsonProperty("strInstructions")]
+        public string strInstructions { get; set; }
+
+        [JsonProperty("strDrinkThumb")]
+        public string strDrinkThumb { get; set; }
+
+        [JsonProperty("strIngredient1")]
+        public string strIngredient1 { get; set; }
+        [JsonProperty("strIngredient2")]
+        public string strIngredient2 { get; set; }
+        [JsonProperty("strIngredient3")]
+        public string strIngredient3 { get; set; }
+        [JsonProperty("strIngredient4")]
+        public string strIngredient4 { get; set; }
+        [JsonProperty("strIngredient5")]
+        public string strIngredient5 { get; set; }
+        [JsonProperty("strIngredient6")]
+        public string strIngredient6 { get; set; }
+        [JsonProperty("strIngredient7")]
+        public string strIngredient7 { get; set; }
+        [JsonProperty("strIngredient8")]
+        public string strIngredient8 { get; set; }
+        [JsonProperty("strIngredient9")]
+        public string strIngredient9 { get; set; }
+        [JsonProperty("strIngredient10")]
+        public string strIngredient10 { get; set; }
+        [JsonProperty("strIngredient11")]
+        public string strIngredient11 { get; set; }
+        [JsonProperty("strIngredient12")]
+        public string strIngredient12 { get; set; }
+        [JsonProperty("strIngredient13")]
+        public string strIngredient13 { get; set; }
+        [JsonProperty("strIngredient14")]
+        public string strIngredient14 { get; set; }
+        [JsonProperty("strIngredient15")]
+        public string strIngredient15 { get; set; }
+
+
+        [JsonProperty("strMeasure1")]
+        public string strMeasure1 { get; set; }
+        [JsonProperty("strMeasure2")]
+        public string strMeasure2 { get; set; }
+        [JsonProperty("strMeasure3")]
+        public string strMeasure3 { get; set; }
+        [JsonProperty("strMeasure4")]
+        public string strMeasure4 { get; set; }
+        [JsonProperty("strMeasure5")]
+        public string strMeasure5 { get; set; }
+        [JsonProperty("strMeasure6")]
+        public string strMeasure6 { get; set; }
+        [JsonProperty("strMeasure7")]
+        public string strMeasure7 { get; set; }
+        [JsonProperty("strMeasure8")]
+        public string strMeasure8 { get; set; }
+        [JsonProperty("strMeasure9")]
+        public string strMeasure9 { get; set; }
+        [JsonProperty("strMeasure10")]
+        public string strMeasure10 { get; set; }
+        [JsonProperty("strMeasure11")]
+        public string strMeasure11 { get; set; }
+        [JsonProperty("strMeasure12")]
+        public string strMeasure12 { get; set; }
+        [JsonProperty("strMeasure13")]
+        public string strMeasure13 { get; set; }
+        [JsonProperty("strMeasure14")]
+        public string strMeasure14 { get; set; }
+        [JsonProperty("strMeasure15")]
+        public string strMeasure15 { get; set; }
+
+        public List<String> ingredientList { get; set; }
+
+
+
+    }
+}
