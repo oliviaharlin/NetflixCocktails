a71618c9317b1863938d02bfed94b7258e8f2c5c
oliviaharlin <oliviaharlin@hotmail.com>
2022-05-10 12:44:51 +0200

    la till helper

---
+++ NetflixCocktails/Helper/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+
+namespace NetflixCocktails.Helper
+{
+    public class Helper
+    {
+
+            public class CocktailAPI {
+
+                public HttpClient Initial() {
+                    var client = new HttpClient();
+
+                    string server = "http://www.thecocktaildb.com";
+                    string relativePath = "api/json/v1/1/random.php";
+
+                    Uri serverUri = new Uri(server);
+                    Uri relativeUri = new Uri(relativePath, UriKind.Relative);
+                    Uri fullUri = new Uri(serverUri, relativeUri);
+
+                    client.BaseAddress = fullUri;
+                    return client;
+                }
+            }
+
+    }
+    
+}
